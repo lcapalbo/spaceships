e78fe8c5e02d83af5e6e29b363cfdcbf435e7cbe
Lucas Capalbo <lucas.capalbo@gmail.com>
2026-03-21 10:05:42 -0300

    Vamos a migrar a game.js los procedimientos que dibujan las naves espaciales. Comencemos con DibujarJugador

--- game.js
+++ game.js
@@ -1,21 +1,65 @@
 const canvas = document.getElementById('gameCanvas');
 const renderer = new Renderer(canvas);
 
+function drawPlayer(x, y, player) {
+  if (player === 1) {
+    renderer.setColor(12);
+  } else {
+    renderer.setColor(11);
+  }
+
+  renderer.line(x - 2, y, x, y - 20);
+  renderer.line(x + 2, y, x, y - 20);
+
+  if (y % 2 === 0) {
+    renderer.setColor(14);
+  } else {
+    renderer.setColor(12);
+  }
+
+  renderer.ellipse(x - 5, y, 180, 0, 3, 5);
+  renderer.ellipse(x + 5, y, 180, 0, 3, 5);
+
+  if (y % 2 === 0) {
+    renderer.setColor(12);
+  } else {
+    renderer.setColor(14);
+  }
+
+  renderer.ellipse(x - 5, y, 180, 0, 1, 5);
+  renderer.ellipse(x + 5, y, 180, 0, 1, 5);
+
+  if (player === 1) {
+    renderer.setColor(14);
+  } else {
+    renderer.setColor(13);
+  }
+
+  renderer.arc(x, y, 0, 180, 8);
+  renderer.arc(x, y, 0, 180, 6);
+
+  if (player === 1) {
+    renderer.setColor(11);
+  } else {
+    renderer.setColor(12);
+  }
+
+  renderer.line(x, y - 20, x + 10, y);
+  renderer.line(x, y - 20, x - 10, y);
+  renderer.line(x - 10, y, x + 10, y);
+
+  renderer.line(x - 10, y - 2, x - 10, y - 9);
+  renderer.line(x - 8, y - 2, x - 10, y - 9);
+  renderer.line(x + 10, y - 2, x + 10, y - 9);
+  renderer.line(x + 8, y - 2, x + 10, y - 9);
+}
+
 function gameLoop() {
   renderer.clearScreen();
 
-  // Ejemplo de uso (puedes comentar/descomentar según lo necesites):
-  renderer.setColor(15);
-  renderer.line(10, 10, 630, 10);
-  renderer.circle(320, 240, 50);
-  renderer.ellipse(320, 240, 0, 360, 80, 40);
-  renderer.setColor(14);
-  renderer.bar(20, 430, 120, 470);
-  renderer.setColor(12);
-  renderer.setFillStyle(1, 12);
-  renderer.pieSlice(540, 100, 0, 120, 30);
-  renderer.setColor(7);
-  renderer.outTextXY(10, 460, 'SpaceShips Retro - API BGI simplificada');
+  // Dibujar ejemplo de jugador 1 y 2
+  drawPlayer(200, 240, 1);
+  drawPlayer(440, 240, 2);
 
   requestAnimationFrame(gameLoop);
 }
